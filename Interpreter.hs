dacf2471f9dec9dabbcb8a06cd18cb3effb9a6ce
Petros Markopoulos <petrosmarkopoulos01@gmail.com>
2020-05-30 03:11:02 -0400

    Edit eval to incorporate alpha reductions

--- Interpreter.hs
+++ Interpreter.hs
@@ -40,9 +40,10 @@ betaReduce wh to = to -- TODO Write real implementation for beta reduction
 eval :: Expression -> Expression
 eval var@(Variable _) = var
 eval abs@(Abstraction c exp) = Abstraction c (eval exp)
-eval app@(Application e1 e2) = let r1 = eval e1
-                                   r2 = eval e2
-                               in
-                                   case r1 of
-                                        abs@(Abstraction _ _) -> eval $ betaReduce r1 r2
-                                        exp                   -> Application r1 r2
+eval app = let app'@(Application e1 e2) = alphaNormalize app
+               r1 = eval e1
+               r2 = eval e2
+           in
+               case r1 of
+                    abs@(Abstraction _ _) -> eval $ betaReduce r1 r2
+                    exp                   -> Application r1 r2
